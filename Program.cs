314cb20ee6c0e46b77caff4121d1c14eb273737a
Ryan Grunest <ryan.grunest@chipotle.com>
2024-06-25 19:44:16 -0600

    init

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+
+Console.WriteLine("Hello " + name);
